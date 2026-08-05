591b8a12af8fdb9872b5626a76c51c8e4a13965d
404-39-Karnkanok <56862@yupparaj.ac.th>
2026-08-05 16:05:59 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
+net_price = price - vat
+net_price = price - vat
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider()
+st.write("นางสาวกาญจน์กนก พูลสวัสดิ์ เลขที่ 39  ม.4/4")
