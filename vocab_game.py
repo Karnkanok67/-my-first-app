661a227f9e2bd0e111507fa8ad81165c5a213557
404-39-Karnkanok <56862@yupparaj.ac.th>
2026-09-02 16:28:45 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -113,7 +113,7 @@ ans4 = st.text_input(
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
 st.session_state.ans3_val = ans3
- st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
